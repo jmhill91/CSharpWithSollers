5f49cfa48c994e60218f33137be648c245b251f2
john hill <john.hill@fdmgroup.com>
2021-06-16 12:06:25 -0700

    day 2 code

--- CSharpWithSollers/Program.cs
+++ CSharpWithSollers/Program.cs
@@ -2,11 +2,29 @@
 
 namespace CSharpWithSollers
 {
+    
     class Program
     {
+        //Field example
+        int nonStaticField;
+        static int staticField;
+
+        //Property Example
+        public object NonStaticProperty { get; set; }
+        public static object StaticProperty { get; set; }
+
+        //Method Example
+        public void NonStaticMEthod() { }
+        public static void StaticMethod() { }
+
+
         static void Main(string[] args)
         {
-            //This is a test
+            //Class Members: field, property,method(non-static(object) static(class))
+
+            Console.WriteLine("Please enter your name"); //output operation
+            string userInput = Console.ReadLine();//David//"David"//Input Operation
+            Console.WriteLine($"Good Morning:{userInput}");
             Console.ReadKey();
         }
     }
